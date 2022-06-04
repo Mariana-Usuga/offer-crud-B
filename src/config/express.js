489de18199a8aff529ea2d7353325f38f5471d6c
Mariana-Usuga <Mariana-Usuga>
2022-06-04 03:25:01 -0500

    change heroku

--- src/config/express.js
+++ src/config/express.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const morgan = require('morgan');
+// const morgan = require('morgan');
 const cors = require('cors');
 
 function connectDB(app) {
   app.use(express.json());
-  app.use(morgan('dev'));
+  // app.use(morgan('dev'));
   app.use(cors());
   app.use(express.urlencoded({ extended: false }))
 }
