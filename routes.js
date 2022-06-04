49d1d6e06eed4d3ce6cba81dd9c295d0639af005
Mariana-Usuga <Mariana-Usuga>
2022-06-04 13:16:42 -0500

    delete nodemodules

--- routes.js
+++ routes.js
@@ -1,11 +1,11 @@
 const offer = require('./src/api/offer');
 const prices = require('./src/api/prices');
 const characteristic = require('./src/api/characteristic')
-const prueba = require('./src/api/prueba')
+// const prueba = require('./src/api/prueba')
 
 function routes(app) {
    app.use('/api/offer', offer);
-   app.use('/', (req, res) => {
+   app.use('/prueba', (req, res) => {
       res.send('Hello World!')
     })
    app.use('/api/price', prices);
