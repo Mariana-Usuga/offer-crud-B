989602d975ccb59b35365b7fb7fa65b25c774479
Mariana-Usuga <Mariana-Usuga>
2022-06-04 11:01:54 -0500

    change heroku

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,7 @@
 const mysql = require('promise-mysql');
 const config = require('./index');
 
+console.log('this is db_uurrlll',  config.db_url)
 
 const connection = mysql.createConnection({
   port: config.db_port,
